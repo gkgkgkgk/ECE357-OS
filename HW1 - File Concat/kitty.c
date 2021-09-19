c772d5a3690967f4673ae40a4ee3f07862cb08d6
gkgkgkgk <gkepets@gmail.com>
2021-09-19 17:08:49 -0400

    fin

--- HW1 - File Concat/kitty.c
+++ HW1 - File Concat/kitty.c
@@ -8,9 +8,11 @@
 
 #define BUF_SIZE 4096
 
-// TODO:
-// Add error handling and test it
-// See if printing to stdout is good because the messages will be in between files.
+void terminate(char *fileName, char *call)
+{
+    fprintf(stderr, "Error! Program Terminated. File: %s | Call: %s\n", fileName, call);
+    exit(-1);
+}
 
 bool checkForBinary(char *buf, int length, char *inputFile)
 {
@@ -35,6 +37,8 @@ void partialWrite(char *buf, int r, int w, int fd, int *writeCount)
         unwrittenBuf[i - w] = buf[i];
     }
 
+    write(fd, unwrittenBuf, r - w);
+
     writeCount++;
 }
 
@@ -42,16 +46,28 @@ void concat(int *outputFile, char *inputFile)
 {
     char buf[BUF_SIZE];
     int fd = strcmp(inputFile, "-") == 0 ? STDIN_FILENO : open(inputFile, O_RDONLY, 0666);
+
+    if (fd < 0)
+        terminate(inputFile, "open");
+
     int r = read(fd, buf, BUF_SIZE);
+
+    if (r < 0)
+        terminate(inputFile, "read");
+
     bool isBinary = false;
     int writeCount = 0;
     int readCount = 1;
+    int bytesWritten = 0;
 
     while (r > 0)
     {
         isBinary = isBinary ? true : checkForBinary(buf, r, inputFile);
         int w = write(*outputFile, buf, r);
+        if (w < 0)
+            terminate("output", "write");
 
+        bytesWritten += w;
         writeCount++;
 
         if (w != r)
@@ -60,28 +76,48 @@ void concat(int *outputFile, char *inputFile)
         }
 
         r = read(fd, buf, BUF_SIZE);
+        if (r < 0)
+            terminate(inputFile, "read");
+
         readCount++;
     }
 
-    //fprintf(stderr, "%s", "hi"); // TODO correct print message here
+    int c = close(fd);
+
+    if (c < 0)
+        terminate(inputFile, "close");
+
+    fprintf(stderr, "Finished concat for file: %s | Bytes transferred: %d | Read count: %d | Write count: %d\n", strcmp(inputFile, "-") == 0 ? "stdin" : inputFile, bytesWritten, readCount, writeCount);
+
+    if (isBinary)
+        fprintf(stderr, "Warning! File %s is a binary file!\n", inputFile);
 }
 
 int main(int argc, char *argv[])
 {
     int c;
-    bool writeOutput = false;
+    int outputFile = STDOUT_FILENO;
 
-    while ((c = getopt(argc, argv, "o::")) != -1)
+    while ((c = getopt(argc, argv, "o:")) != -1)
     {
         if (c == 'o')
-            writeOutput = true;
+        {
+            outputFile = open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if (outputFile < 0)
+                terminate("output", "open");
+        }
     }
 
-    int outputFile = writeOutput ? open("file3", O_WRONLY | O_CREAT | O_TRUNC, 0666) : STDOUT_FILENO;
-
-    for (int i = optind; i < argc; i++)
+    if (optind >= argc)
     {
-        concat(&outputFile, argv[i]);
+        concat(&outputFile, "-");
+    }
+    else
+    {
+        for (int i = optind; i < argc; i++)
+        {
+            concat(&outputFile, argv[i]);
+        }
     }
 
     return 0;

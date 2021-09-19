ee8b5f767f6d2e05b55acaa8853ec077ef92ac9e
gkgkgkgk <gkepets@gmail.com>
2021-09-19 17:29:11 -0400

    cleanup more

--- HW1 - File Concat/kitty.c
+++ HW1 - File Concat/kitty.c
@@ -10,8 +10,16 @@
 
 void terminate(char *fileName, char *call)
 {
-    fprintf(stderr, "Error! Program Terminated. File: %s | Call: %s\n", fileName, call);
+    fprintf(stderr, "Error! Program Terminated. File: %s | SysCall: %s\n", fileName, call);
     exit(-1);
+}
+
+int mySyscall(int n, char *fileName, char *type)
+{
+    if (n < 0)
+        terminate(fileName, type);
+
+    return n;
 }
 
 bool checkForBinary(char *buf, int length, char *inputFile)
@@ -37,7 +45,7 @@ void partialWrite(char *buf, int r, int w, int fd, int *writeCount)
         unwrittenBuf[i - w] = buf[i];
     }
 
-    write(fd, unwrittenBuf, r - w);
+    mySyscall(write(fd, unwrittenBuf, r - w), "output", "write");
 
     writeCount++;
 }
@@ -45,16 +53,8 @@ void partialWrite(char *buf, int r, int w, int fd, int *writeCount)
 void concat(int *outputFile, char *inputFile)
 {
     char buf[BUF_SIZE];
-    int fd = strcmp(inputFile, "-") == 0 ? STDIN_FILENO : open(inputFile, O_RDONLY, 0666);
-
-    if (fd < 0)
-        terminate(inputFile, "open");
-
-    int r = read(fd, buf, BUF_SIZE);
-
-    if (r < 0)
-        terminate(inputFile, "read");
-
+    int fd = strcmp(inputFile, "-") == 0 ? STDIN_FILENO : mySyscall(open(inputFile, O_RDONLY, 0666), inputFile, "open");
+    int r = mySyscall(read(fd, buf, BUF_SIZE), inputFile, "read");
     bool isBinary = false;
     int writeCount = 0;
     int readCount = 1;
@@ -63,9 +63,7 @@ void concat(int *outputFile, char *inputFile)
     while (r > 0)
     {
         isBinary = isBinary ? true : checkForBinary(buf, r, inputFile);
-        int w = write(*outputFile, buf, r);
-        if (w < 0)
-            terminate("output", "write");
+        int w = mySyscall(write(*outputFile, buf, r), "output", "write");
 
         bytesWritten += w;
         writeCount++;
@@ -75,18 +73,11 @@ void concat(int *outputFile, char *inputFile)
             partialWrite(buf, r, w, *outputFile, &writeCount);
         }
 
-        r = read(fd, buf, BUF_SIZE);
-        if (r < 0)
-            terminate(inputFile, "read");
-
+        r = mySyscall(read(fd, buf, BUF_SIZE), inputFile, "read");
         readCount++;
     }
 
-    int c = close(fd);
-
-    if (c < 0)
-        terminate(inputFile, "close");
-
+    int c = mySyscall(close(fd), inputFile, "close");
     fprintf(stderr, "Finished concat for file: %s | Bytes transferred: %d | Read count: %d | Write count: %d\n", strcmp(inputFile, "-") == 0 ? "stdin" : inputFile, bytesWritten, readCount, writeCount);
 
     if (isBinary)
@@ -102,9 +93,7 @@ int main(int argc, char *argv[])
     {
         if (c == 'o')
         {
-            outputFile = open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-            if (outputFile < 0)
-                terminate("output", "open");
+            outputFile = mySyscall(open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0666), "output", "open");
         }
     }
 
